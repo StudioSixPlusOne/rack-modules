a78563f6ab55e827f8b373f8a7c6e08d0d2e654b
Curlymorphic <dave.french3@googlemail.com>
2020-06-16 15:20:28 +0100

    Zazel only display easeinout in widget

--- src/modules/Zazel.cpp
+++ src/modules/Zazel.cpp
@@ -230,9 +230,7 @@ struct EasingWidget : Widget
         nvgMoveTo (args.vg, mm2px (border), mm2px (border + width));
         for (auto i = 0.0f; i < 1.0f; i += 0.01f)
         {
-            auto easingY = module->getOneShot()
-                               ? easing->easeIn (i, 0.0f, 1.0f, 1.0f)
-                               : easing->easeInOut (i, 0.0f, 1.0f, 1.0f);
+            auto easingY = easing->easeInOut (i, 0.0f, 1.0f, 1.0f);
             nvgLineTo (args.vg,
                        mm2px (permm * i + border),
                        mm2px (border + width - width * easingY));
