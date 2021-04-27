d368cb458d58f405fbec9425b9191bb8200ca562
Curlymorphic <dave.french3@googlemail.com>
2021-04-28 00:47:59 +0100

    Zilah, define uint8_t uint16_t

--- src/modules/Zilah.cpp
+++ src/modules/Zilah.cpp
@@ -1,6 +1,28 @@
-#include "plugin.hpp"
-#include <stdint.h>
+/*
+ * Copyright (c) 2021 Dave French <contact/dot/dave/dot/french3/at/googlemail/dot/com>
+ *
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ *
+ */
 
+#include "plugin.hpp"
+
+typedef unsigned char uint8_t;
+typedef unsigned short int uint16_t;
 //
 
 // Midi cc is 7 bit, double is 14 bit
