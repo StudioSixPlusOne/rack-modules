0d123721284a8f0d719bae890eb2556cf9e56713
curlymorphic <dave.french3@googlemail.com>
2023-02-12 19:03:28 +0000

    Github  ci unit tests on linux12

--- test/main.cpp
+++ test/main.cpp
@@ -118,7 +118,7 @@ int main (int argc, char** argv)
     testCircularBuffer();
     testLookupTable();
     testAnalyzer();
-    testPolyShiftRegister();
+//    testPolyShiftRegister();
     testKSDelay();
     testCombFilter();
     testMaccomo();
