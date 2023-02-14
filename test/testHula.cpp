7caa3a6a23cf8f1ef930d3890aafc785c19a38eb
curlymorphic <dave.french3@googlemail.com>
2023-02-14 07:35:00 +0000

    Hula, updated unit tests

--- test/testHula.cpp
+++ test/testHula.cpp
@@ -29,6 +29,7 @@
 #include "Analyzer.h"
 #include "testSignal.h"
 #include "FftAnalyzer.h"
+#include <cmath>
 
 #include "../src/composites/Hula.h"
 
@@ -97,13 +98,15 @@ static void testVoct (float_4 vocts, float sr)
     hc.inputs[HC::FM_INPUT].setChannels (1);
     hc.inputs[HC::DEPTH_CV_INPUT].setChannels (1);
 
-    hc.params[HC::RATIO_PARAM].setValue (1.0f);
+    hc.params[HC::RATIO_PARAM].setValue (0.0f);
     hc.params[HC::SEMITONE_PARAM].setValue (0.0f);
     hc.params[HC::OCTAVE_PARAM].setValue (0.0f);
     hc.params[HC::DEPTH_PARAM].setValue (0.0f);
     hc.params[HC::FEEDBACK_PARAM].setValue (0.0f);
+    hc.params[HC::DEFAULT_TUNING_PARAM].setValue (261.62f);
 
-    hc.inputs[HC::FEEDBACK_CV_INPUT].setVoltage (0.0f, 0);
+    hc.inputs[HC::FEEDBACK_CV_INPUT]
+        .setVoltage (0.0f, 0);
     hc.inputs[HC::FM_INPUT].setVoltage (0.0f, 0);
     hc.inputs[HC::DEPTH_CV_INPUT].setVoltage (0.0f, 0);
     hc.inputs[HC::VOCT_INPUT].setVoltageSimd (vocts, 0);
@@ -173,10 +176,11 @@ static void testOctave (float octave, float sr)
     hc.inputs[HC::FM_INPUT].setChannels (1);
     hc.inputs[HC::DEPTH_CV_INPUT].setChannels (1);
 
-    hc.params[HC::RATIO_PARAM].setValue (1.0f);
+    hc.params[HC::RATIO_PARAM].setValue (0.0f);
     hc.params[HC::SEMITONE_PARAM].setValue (0.0f);
     hc.params[HC::DEPTH_PARAM].setValue (0.0f);
     hc.params[HC::FEEDBACK_PARAM].setValue (0.0f);
+    hc.params[HC::DEFAULT_TUNING_PARAM].setValue (261.63f);
 
     hc.params[HC::OCTAVE_PARAM].setValue (octave);
 
@@ -244,10 +248,11 @@ static void testRatio (float ratio, float sr)
     hc.inputs[HC::FM_INPUT].setChannels (1);
     hc.inputs[HC::DEPTH_CV_INPUT].setChannels (1);
 
-    hc.params[HC::RATIO_PARAM].setValue (ratio);
+    hc.params[HC::RATIO_PARAM].setValue (std::log2f (ratio));
     hc.params[HC::SEMITONE_PARAM].setValue (0.0f);
     hc.params[HC::DEPTH_PARAM].setValue (0.0f);
     hc.params[HC::FEEDBACK_PARAM].setValue (0.0f);
+    hc.params[HC::DEFAULT_TUNING_PARAM].setValue (261.63f);
 
     hc.params[HC::OCTAVE_PARAM].setValue (-0.0f);
 
@@ -322,6 +327,7 @@ static void testDcOffset (float_4 vocts, float sr)
     hc.params[HC::OCTAVE_PARAM].setValue (0.0f);
     hc.params[HC::DEPTH_PARAM].setValue (0.0f);
     hc.params[HC::FEEDBACK_PARAM].setValue (0.0f);
+    hc.params[HC::DEFAULT_TUNING_PARAM].setValue (261.63f);
 
     hc.inputs[HC::FEEDBACK_CV_INPUT].setVoltage (0.0f, 0);
     hc.inputs[HC::FM_INPUT].setVoltage (0.0f, 0);
