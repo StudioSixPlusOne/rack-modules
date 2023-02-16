03432aa15ab6fd81154b37f6eaea6713edd1879d
curlymorphic <dave.french3@googlemail.com>
2023-02-16 19:09:10 +0000

    Hula, unit tests required additional parameters setup

--- test/testHula.cpp
+++ test/testHula.cpp
@@ -104,6 +104,9 @@ static void testVoct (float_4 vocts, float sr)
     hc.params[HC::DEPTH_PARAM].setValue (0.0f);
     hc.params[HC::FEEDBACK_PARAM].setValue (0.0f);
     hc.params[HC::DEFAULT_TUNING_PARAM].setValue (261.62f);
+    hc.params[HC::UNISON_PARAM].setValue (1.0f);
+    hc.params[HC::DC_OFFSET_PARAM].setValue (0.0f);
+    hc.params[HC::SCALE_PARAM].setValue (1.0f);
 
     hc.inputs[HC::FEEDBACK_CV_INPUT]
         .setVoltage (0.0f, 0);
@@ -133,7 +136,7 @@ static void testVoct (float_4 vocts, float sr)
         auto maxResponseBin = Analyzer::getMax (response);
         auto minRequiredBin = FFT::freqToBin (minFreqs[i], sr, size);
         auto maxRequiredBin = FFT::freqToBin (maxFreqs[i], sr, size) + 2;
-#if 0
+#if 1
         printf ("SR: %f   Voct: %f   Freq: %f  Min Expected bin: %d  Mxn Expected bin: %d   Actual bin: %d  Pass %d\n",
                 sr,
                 vocts[i],
@@ -181,6 +184,9 @@ static void testOctave (float octave, float sr)
     hc.params[HC::DEPTH_PARAM].setValue (0.0f);
     hc.params[HC::FEEDBACK_PARAM].setValue (0.0f);
     hc.params[HC::DEFAULT_TUNING_PARAM].setValue (261.63f);
+    hc.params[HC::UNISON_PARAM].setValue (1.0f);
+    hc.params[HC::DC_OFFSET_PARAM].setValue (0.0f);
+    hc.params[HC::SCALE_PARAM].setValue (1.0f);
 
     hc.params[HC::OCTAVE_PARAM].setValue (octave);
 
@@ -253,6 +259,9 @@ static void testRatio (float ratio, float sr)
     hc.params[HC::DEPTH_PARAM].setValue (0.0f);
     hc.params[HC::FEEDBACK_PARAM].setValue (0.0f);
     hc.params[HC::DEFAULT_TUNING_PARAM].setValue (261.63f);
+    hc.params[HC::UNISON_PARAM].setValue (1.0f);
+    hc.params[HC::DC_OFFSET_PARAM].setValue (0.0f);
+    hc.params[HC::SCALE_PARAM].setValue (1.0f);
 
     hc.params[HC::OCTAVE_PARAM].setValue (-0.0f);
 
@@ -328,6 +337,9 @@ static void testDcOffset (float_4 vocts, float sr)
     hc.params[HC::DEPTH_PARAM].setValue (0.0f);
     hc.params[HC::FEEDBACK_PARAM].setValue (0.0f);
     hc.params[HC::DEFAULT_TUNING_PARAM].setValue (261.63f);
+    hc.params[HC::UNISON_PARAM].setValue (1.0f);
+    hc.params[HC::DC_OFFSET_PARAM].setValue (0.0f);
+    hc.params[HC::SCALE_PARAM].setValue (1.0f);
 
     hc.inputs[HC::FEEDBACK_CV_INPUT].setVoltage (0.0f, 0);
     hc.inputs[HC::FM_INPUT].setVoltage (0.0f, 0);
