55d07af45cbfb43b010018c49196abad10717730
curlymorphic <dave.french3@googlemail.com>
2023-02-27 15:02:19 +0000

    Lala Stereo, change parameter knob to show frequency

--- src/composites/LalaStereo.h
+++ src/composites/LalaStereo.h
@@ -181,7 +181,7 @@ inline void LalaStereoComp<TBase>::step()
 
         //simd'ed out = std::isfinite (out) ? out : 0;
         lowOut = rack::simd::ifelse ((movemask (lowOut == lowOut) != 0xF), float_4 (0.0f), lowOut);
-        highOut = rack::simd::ifelse ((movemask (highOut == highOut) != 0xF), float_4 (0.0f), highOut)
+        highOut = rack::simd::ifelse ((movemask (highOut == highOut) != 0xF), float_4 (0.0f), highOut);
 
         lowOut.store (TBase::outputs[LEFT_LOW_OUTPUT].getVoltages (c));
         highOut.store (TBase::outputs[LEFT_HIGH_OUTPUT].getVoltages (c));
@@ -204,7 +204,6 @@ inline void LalaStereoComp<TBase>::step()
 
         lowOut = rack::simd::ifelse ((movemask (lowOut == lowOut) != 0xF), float_4 (0.0f), lowOut);
         highOut = rack::simd::ifelse ((movemask (highOut == highOut) != 0xF), float_4 (0.0f), highOut);
-
 
         lowOut.store (TBase::outputs[RIGHT_LOW_OUTPUT].getVoltages (c));
         highOut.store (TBase::outputs[RIGHT_HIGH_OUTPUT].getVoltages (c));
@@ -232,11 +231,11 @@ IComposite::Config LalaStereoDescription<TBase>::getParam (int i)
     switch (i)
     {
         case LalaStereoComp<TBase>::FREQ_PARAM:
-            ret = { 0.0f, 1.0f, 0.5f, "FREQ", " ", 0.0f, 1.0f, 0.0f };
+            ret = { 0.0f, 1.125f, 0.5f, "Frequency", " Hz", freqBase, freqMul };
             break;
 
         case LalaStereoComp<TBase>::FREQ_CV_PARAM:
-            ret = { 0.0f, 1.0f, 0.5f, "FREQ_CV", " ", 0.0f, 1.0f, 0.0f };
+            ret = { -1.0f, 1.0f, 0.0f, "Frequency CV", " ", 0.0f, 1.0f, 0.0f };
             break;
 
         default:
